e8bd1e38b2149e6ebbc013c9f9f6b7621ee646ed
sudhir <sudhir9297@gmail.com>
2025-05-02 15:15:13 +0530

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
 import { GalleryVerticalEnd } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
